c5772c14f0e08f8fb5ee838f3f2a0c4334360f4d
mda153 <102181098+mda153@users.noreply.github.com>
2022-04-13 17:00:08 +1200

    Update get_m_curv_pre1930_conc04_rev1.py

--- get_m_curv_pre1930_conc04_rev1.py
+++ get_m_curv_pre1930_conc04_rev1.py
@@ -10,30 +10,31 @@ import numpy as np
 import o3seespy.extensions
 
 
-def get_moment_curvature(axial_load=0, max_curve=0.001, num_incr=500):
+def get_moment_curvature(axial_load=0, max_curve=0.0001, num_incr=500):
     osi = o3.OpenSeesInstance(ndm=2, ndf=3, state=3)
 
-    f_c28 = 2.466  #28 day compressive strength [ksi]
-    e_mod = 57000.0 * np.sqrt(f_c28 * 1000.0) / 1e3 #Is used here in the Concrete 04 model => Concrete 01 does not account for this (concrete modulus)
-    f_conc_tens = 0.36 * np.sqrt(f_c28)  # NZS3101 CL 5.2.6, concrte tensile strength
-    eps_conc_tens = f_conc_tens / e_mod  # Park and Paulay 1975 assume linear
+    f_c28 = 17*1E3 #[kPa] 28 day compressive strength
+    e_mod_conc = (3320*np.sqrt(f_c28) + 6900)*1E3 # [kPa] NZS3101 - 2006
+    #f_conc_tens = (0.38 * np.sqrt(f_c28))  # [kPa] NZS3101 CL 5.2.4, direct tensile strength concrete
+    f_conc_tens = 1.4*1E3 #[kPa] Defined as this for this section in Henderson Thesis
+    eps_conc_tens = f_conc_tens / e_mod_conc  # Park and Paulay 1975 assume linear
     eps_unconf_conc_max = 0.004  #Ultimate strain for unconfined concrete Priestly et al. 2007, lower bound (0.004 - 0.005)
 
-    eps_conc_crush = 0.03  #Concrte strain at crushing - Koopaee (2015) from NZS 3101?? taken from Fig 2.1 fc=20MPa curve
-    
+    eps_conc_crush = 0.003  #Concrte strain at crushing - Koopaee (2015) from NZS 3101?? taken from Fig 2.1 fc=20MPa curve
     
 
-    conc_unconf = o3.uniaxial_material.Concrete04(osi, fc=-f_c28, epsc=-eps_unconf_conc_max, epscu=-eps_conc_crush, ec=e_mod, fct=f_conc_tens, et=eps_conc_tens) #unconfined concrte properties
+
+    conc_unconf = o3.uniaxial_material.Concrete04(osi, fc=-f_c28, epsc=-eps_unconf_conc_max, epscu=-eps_conc_crush, ec=e_mod_conc, fct=f_conc_tens, et=eps_conc_tens) #unconfined concrte properties
  
     #Defining concrete beam parameters
 
-    h = 23.622 #Section height
-    b = 5.906 #Section width
+    h = .600 #Section height [m]
+    b = .150 #Section width [m]
 
     gj = 1.0E10 # linear elastic torsional stiffness
    
-    nf_y = 2 #number of fibers in Y-Y dir
-    nf_z = 2 #number of fibers in Z-Z dir
+    nf_y = 20 #number of fibers in Y-Y dir - (10 - 20 is a good number here)
+    nf_z = 20 #number of fibers in Z-Z dir
  
     
     
@@ -100,8 +101,8 @@ if __name__ == '__main__':
     mom, curve = get_moment_curvature()
     plt.plot(curve, mom)
     axes = plt.axes()
-    axes.set_xlabel("Curvature [1/in^2]")
-    axes.set_ylabel("Bending Moment [kip*in]")
+    axes.set_xlabel("Curvature [1/m2]")
+    axes.set_ylabel("Moment [kNm]")
     
     
 
